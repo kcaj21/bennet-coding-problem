a61091fb8d68129107aa5acf7e1783eac410c23a
kcaj21 <alex.kcaj@gmail.com>
2024-05-25 16:11:22 +0100

    part 2 done

--- python/optool.py
+++ python/optool.py
@@ -2,23 +2,29 @@ import requests
 import sys
 import pdb
 
+
 def main(chemical_code):
     name = get_chemical_name(chemical_code)
     print(name)
-
+    
+    icb_spending_data = get_icb_spending_data(chemical_code)
+    for row in icb_spending_data:
+        print(row)
 
 def get_chemical_name(chemical_code):
     
+    # checks the chemical code entered is 9 characters long
     if not (len(chemical_code) == 9):
         raise ValueError("must be 9 character chemical code")
     
+    # fetches the chemical name from the api
     url = "https://openprescribing.net/api/1.0/bnf_code"
     resp = requests.get(url, params={"q": chemical_code, "exact": "true", "format": "json"})
-    # pdb.set_trace()
     resp.raise_for_status()
     
     results = resp.json()
     
+    # checks if there are any results returned from the get request
     if (len(results) == 0):
         raise ValueError("not found")
         
@@ -27,7 +33,20 @@ def get_chemical_name(chemical_code):
 
     return result["name"]
 
-
+def get_icb_spending_data(chemical_code):
+    
+    # fetches the spending data from the api
+    url = "https://openprescribing.net/api/1.0/spending_by_org/?"
+    resp = requests.get(url, params={"code": chemical_code, "format": "json", "org_type": "icb"})
+    resp.raise_for_status()
+    
+    results = resp.json()
+    
+    #creating new list through list comprehension, only including the date, icb and amount properties    
+    spend_data = [res["date"] + " " + res["row_name"] + " " + str(res["items"]) for res in results]
+    
+    return spend_data
+    
 if __name__ == "__main__":
     chemical_code = sys.argv[1]
     main(chemical_code)
--- python/test_optool.py
+++ python/test_optool.py
@@ -40,3 +40,42 @@ def test_code_not_present():
     with pytest.raises(ValueError) as excinfo:
         optool.get_chemical_name("0000000AA")
     assert "not found" in str(excinfo.value)
+    
+# @pytest.mark.xfail(reason="not implemented")
+@responses.activate
+def test_get_icb_spending_data():
+    responses.add(
+        responses.GET,
+        "https://openprescribing.net/api/1.0/spending_by_org/?org_type=icb&code=0407010AD&format=json",
+        json=[
+            {
+                "items": 1,
+                "quantity": 68,
+                "actual_cost": 10.3,
+                "date": "2019-04-01",
+                "row_id": "QH8",
+                "row_name": "NHS MID AND SOUTH ESSEX INTEGRATED CARE BOARD",
+            },
+            {
+                "items":1,
+                "quantity":12.0,
+                "actual_cost":2.41,
+                "date":"2019-04-01",
+                "row_id":"QHL",
+                "row_name":"NHS BIRMINGHAM AND SOLIHULL INTEGRATED CARE BOARD"
+            },
+            {
+                "items":2,
+                "quantity":92.0,
+                "actual_cost":13.93,
+                "date":"2019-04-01",
+                "row_id":"QJ2",
+                "row_name":"NHS DERBY AND DERBYSHIRE INTEGRATED CARE BOARD"
+            },
+        ],
+    )
+    assert optool.get_icb_spending_data("0407010AD") == [
+        '2019-04-01 NHS MID AND SOUTH ESSEX INTEGRATED CARE BOARD 1',
+        '2019-04-01 NHS BIRMINGHAM AND SOLIHULL INTEGRATED CARE BOARD 1', 
+        '2019-04-01 NHS DERBY AND DERBYSHIRE INTEGRATED CARE BOARD 2'
+        ]
