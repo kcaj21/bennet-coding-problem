f401d367b76b432af25ceb876f58ad5f12d92beb
kcaj21 <alex.kcaj@gmail.com>
2024-05-27 22:03:43 +0100

    moved filtering logic into its own function

--- python/optool.py
+++ python/optool.py
@@ -48,32 +48,39 @@ def get_icb_spending_data(chemical_code):
     params = {"code": chemical_code, "format": "json", "org_type": "icb"}   
     results = api_fetch(url, params).json()
     
+    #creating a new list that filters data for which ICBs have prescribed the chemical most frequently, per date
+    new_list = filter_spending_data(results)
+
+    #creating the final spend_data list through list comprehension, only including the date, icb and amount properties    
+    spend_data = [res["date"] + " " + res["row_name"] + " " + str(res["items"]) for res in new_list]
+
+    return spend_data
+
+def filter_spending_data(data):
+    
     #sorting the results by items 1st, date 2nd, so the first object per date will contain highest items value
-    results.sort(key=lambda x: x["items"], reverse=True)
-    results.sort(key=lambda x: x["date"], reverse=True)
+    data.sort(key=lambda x: x["items"], reverse=True)
+    data.sort(key=lambda x: x["date"], reverse=True)
     
     #creating a list of the unique dates from the json data
-    unique_dates = {res["date"] for res in results}
+    unique_dates = {res["date"] for res in data}
     
-    #instantiating a new list to add only the ojects with the highest items value per date
     new_list = []
     
     #using the unique dates to iterate over the json data
     for date in unique_dates:
 
         #per iteration, filtering for objects by date and adding them to a temporary list
-        filtered_list = list(filter(lambda x: x["date"] == date, results))
+        filtered_list = list(filter(lambda x: x["date"] == date, data))
         
         #filtering the temporary list for objects where the items value is equal to the highest items value adding the result of the iteration to the new_list variable (I'm not simply returning the first record, because there could be more than one icb per date sharing the same items value)
         new_list.extend(filter(lambda x: x["items"] == filtered_list[0]["items"],filtered_list))
         
     #sorting again by date, as the unique_dates variable is an unordered set
     new_list.sort(key=lambda x: x["date"], reverse=True)
-
-    #creating the final spend_data list through list comprehension, only including the date, icb and amount properties    
-    spend_data = [res["date"] + " " + res["row_name"] + " " + str(res["items"]) for res in new_list]
-
-    return spend_data
+    
+    return new_list
+    
     
 if __name__ == "__main__":
     chemical_code = sys.argv[1]
